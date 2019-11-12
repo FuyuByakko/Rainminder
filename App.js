f451f86a1945b669a4e0df2b9cf73897070a3007
Igor Michailov <igor.m.byak@gmail.com>
2019-11-12 23:51:20 +0900

    added some logic for the time calculations

--- App.js
+++ App.js
@@ -5,7 +5,7 @@
 const {openweather} = require("./server/APIS");
 
 import React, {Component} from 'react';
-import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, Button } from 'react-native';
+import { SafeAreaView, StyleSheet, ScrollView, View, Text, Picker, Button } from 'react-native';
 import axios from "axios";
 import GetLocation from 'react-native-get-location';
 import parseWeather from './src/utils/parseWeather.js';
@@ -14,13 +14,16 @@ import UserLocation from './src/components/UserLocation';
 
 export default class App extends Component {
   state = {
-    date: new Date(),
+    date: "",
     rainyDays: [],
     weatherToday: "Check the Weather",
     weatherStatuses: {
       checking: "Getting the Weather",
       done: "Today's weather is: ",
     },
+    selectedHour: "00",
+    selectedMinutes: "00",
+    warningTime: "00"
   }
 
   render() {
@@ -30,18 +33,41 @@ export default class App extends Component {
           <Text>Rain-minder</Text>
         </View>
         <View>
-          <Text> {} </Text>
-        </View>
-        <View>
           <Button title="check weather!" onPress={this.getWeather} />
           <Text> {this.state.weatherToday} </Text>
           {/* {{ for(entry of this.state.rainyDays) {
             <Text>{this.state.rainyDays}</Text>
           }
-          }} */}
+        }} */}
 
           {/* <UserLocation propTest='props passed' /> */}
         </View>
+        <Text> SELECT THE TIME YOU WILL LEAVE HOME: {this.state.selectedHour + ":" + this.state.selectedMinutes}</Text>
+        <View style={{flexDirection:"row"}}>
+          <Picker
+            selectedValue={this.state.selectedHour}
+            style={{height:50, width: 50, backgroundColor: '#f9c2ff', padding: 20, marginVertical: 8, marginHorizontal: 16}}
+            onValueChange={itemValue => this.setState({selectedHour: itemValue})}>
+            <Picker.Item label="00" value="00" />
+            <Picker.Item label="05" value="05" />
+            <Picker.Item label="06" value="06" />
+            <Picker.Item label="07" value="07" />
+            <Picker.Item label="08" value="08" />
+            <Picker.Item label="09" value="09" />
+            <Picker.Item label="23" value="23" />
+          </Picker>
+          <Picker
+            selectedValue={this.state.selectedMinutes}
+            style={{height:50, width: 50, backgroundColor: '#f9c2ff', padding: 20, marginVertical: 8, marginHorizontal: 16}}
+            onValueChange={itemValue => this.setState({selectedMinutes: itemValue})}>
+            <Picker.Item label="01" value="01" />
+            <Picker.Item label="15" value="15" />
+            <Picker.Item label="30" value="30" />
+            <Picker.Item label="45" value="45" />
+          </Picker>
+        </View>
+        <Button title="SET TIMER!" onPress={this.setCheck} />
+        <Text> You will be warned {this.state.warningTime} minutes in advance. </Text>
       </SafeAreaView>
     );
   }
@@ -49,6 +75,31 @@ export default class App extends Component {
   componentDidMount() {
     this.setState({weatherToday: this.state.weatherStatuses.checking})
     this.getWeather();
+  }
+
+  setCheck = () => {
+    // 5minutes * 60 secs = 300 secs * 1000 ms = 300000
+    const date = new Date(this.state.date)
+    const offset = date.getTimezoneOffset() / 60;
+    const year = date.getUTCFullYear();
+    const month = date.getUTCMonth();
+    const day = date.getUTCDate();
+    console.log( date, year, month, day, offset)
+
+    const selectedTime = Date.UTC(
+      year,
+      month,
+      day,
+      Number(this.state.selectedHour) + offset,
+      Number(this.state.selectedMinutes) + offset,
+    );
+    const timeToWarn = selectedTime - this.state.warningTime * 60 * 1000;
+    console.log("Time To Warn: " + timeToWarn);
+    console.log("Time Now: " + date);
+
+    setTimeout(() => {
+      console.log("WAKE UUUUUUUUUUUUUUUUUP")
+    }, timeToWarn - Date.now() );
   }
 
   getLoc = async () => {
@@ -82,9 +133,9 @@ export default class App extends Component {
       const parsedWeatherPoints = parseWeather(AllWeatherPoints.data);
 
       this.setState({rainyDays: parsedWeatherPoints});
-      
-      const today = new Date();
-      
+
+      const today = Date.now();
+
       // NEED TO CHANGE FILTERING LOGIC
       // const weatherByDay = parsedWeatherPoints.reduce((allDays, curDay) => {
       //   let curDayDate = new Date(curDay.date * 1000).getUTCDate();
@@ -93,10 +144,10 @@ export default class App extends Component {
       //   }
       //   return allDays;
       // }, []);
-      
+
       const worstToday = parsedWeatherPoints.reduce((thisDay, curPoint) => {
         let curPointDate = new Date(curPoint.date * 1000).getUTCDate();
-        let todayDate = today.getUTCDate();
+        let todayDate = today;
         if (curPointDate === todayDate) {
           console.log("comparison passed");
         // 2 > 5 > 3
@@ -114,9 +165,9 @@ export default class App extends Component {
       }, {});
 
       if (Object.keys(worstToday).length < 1) {
-        this.setState({weatherToday: this.state.weatherStatuses.done.concat("FINE!")})
+        this.setState({weatherToday: this.state.weatherStatuses.done.concat("FINE!"), date: today})
       } else {
-        this.setState({weatherToday: this.state.weatherStatuses.done.concat(worstToday.weather)});
+        this.setState({weatherToday: this.state.weatherStatuses.done.concat(worstToday.weather), date: today});
       }
 
     } catch (e){
